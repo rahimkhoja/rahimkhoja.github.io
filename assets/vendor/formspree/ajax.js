0888483a3929146593acea4c44ca37a4d3b461f3
rahimkhoja <rahimkhoja@users.noreply.github.com>
2024-08-20 05:38:02 +0000

    deploy: 1e259330976802cf69bd1e1e000ddb618c99eedf

---
+++ assets/vendor/formspree/ajax.js
@@ -0,0 +1,41 @@
+  var form = document.getElementById("ajax-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.querySelector(".sent-message");
+    var error = document.querySelector(".error-message");
+    var loading = document.querySelector(".loading");
+    var data = new FormData(event.target);
+    
+    loading.style.display = "block"; // Show loading indicator
+    
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      loading.style.display = "none"; // Hide loading indicator
+      if (response.ok) {
+        status.style.display = "block";
+        status.textContent = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            error.textContent = data["errors"].map(error => error["message"]).join(", ");
+          } else {
+            error.textContent = "Oops! There was a problem submitting your form";
+          }
+          error.style.display = "block";
+        })
+      }
+    }).catch(error => {
+      loading.style.display = "none"; // Hide loading indicator
+      error.textContent = "Oops! There was a problem submitting your form";
+      error.style.display = "block";
+    });
+  }
+  
+  form.addEventListener("submit", handleSubmit);
